83b208e00dca35e14fceaf9d061ef4a038c757cb
yimkh <1256815659@qq.com>
2021-05-21 11:59:32 +0800

    update
    delete some annotation

--- render/node_info/node_info.js
+++ render/node_info/node_info.js
@@ -1,7 +1,3 @@
-// const console = require("console");
-
-// console.log("this is in node_info.js")
-
 let node_info_btn = this.document.querySelector('#node_info_btn')
 node_info_btn.onclick = () => {
     let node_info_content = this.document.querySelector("#node_info_content")
@@ -10,6 +6,5 @@ node_info_btn.onclick = () => {
 
 var node_info_detail_btn = this.document.querySelector('#node_info_detail_btn')
 node_info_detail_btn.onclick = () => {
-    // console.log("node_info_detail is coming")
     window.open("node_info_detail.html")
 }
