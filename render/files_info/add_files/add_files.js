f4334ff7288206f5495d3daf45e7d5ee5bf5959e
yimkh <1256815659@qq.com>
2021-05-25 10:40:49 +0800

    ADD send get request to server
    file_path in readAsText has error

--- render/files_info/add_files/add_files.js
+++ render/files_info/add_files/add_files.js
@@ -1,3 +1,4 @@
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 const { ipcRenderer } = require('electron')
 
 let add_file_btn = document.querySelector("#add_file_btn")
@@ -8,34 +9,42 @@ add_file_btn.onclick = () => {
     let file_name = file[0]['name']
     let file_path = file[0]['path']
 
+    console.log("file[0]", file[0])
+
     //read file
     //then chack file content , and return a file_mark 0/1
     //then send file_info to main processor
     let reader = new FileReader()
     reader.readAsText(file[0]);
     reader.onload = function fileReadCompleted() {
-        /*将文件进行检查的逻辑，返回标志位 file_mark --------------------------!
+        console.log("reader.result", reader.result)
+
+        //check
         const check = new XMLHttpRequest()
-        const url = ""
-        down_https.open("GET", url)
-        down_https.send()
+        const url = "http://localhost:8080/ping/"+reader.result
 
-        down_https.onreadystatechange = (e) => {
-            console.log(down_https)
+        check.open("GET", url, true)
+
+        let file_mark = 2 
+        let file_content = reader.result
+
+        check.onreadystatechange = function() {
+            if ( check.status == 200 && check.readyState == 4){
+                let file_message_from_http = JSON.parse(check.responseText)
+                file_mark = file_message_from_http["mark"]
+                file_content = file_message_from_http["message"]
+
+                //send file_info to main.js
+                let file_info = {"file_mark":file_mark, "file_path": file_path, "file_relative_path": file_relative_path, "file_name":file_name, "file_content": file_content}
+                console.log("file_info", file_info) 
+
+                let encrypt_ipfs_file_path = ipcRenderer.sendSync('add_file_info_message', file_info)
+
+                let files_info_content = document.querySelector("#files_info_content")
+                files_info_content.innerHTML = encrypt_ipfs_file_path
+            }
         }
-        */
-
-        let file_mark = 0
-        if (reader.result=="不合法") {
-            file_mark = 1
-        }
-
-        //send file_info
-        let file_info = {"file_mark":file_mark, "file_path": file_path, "file_relative_path": file_relative_path, "file_name":file_name, "file_content": reader.result}
-
-        let encrypt_ipfs_file_path = ipcRenderer.sendSync('add_file_info_message', file_info)
-
-        let files_info_content = document.querySelector("#files_info_content")
-        files_info_content.innerHTML = encrypt_ipfs_file_path
+        
+        check.send(null);
     };
 }
